f1171453ff9e3c5a2d03cd490723edf237e4be66
Alexander Constanti <aconstanti@tyro.com>
2021-08-23 11:47:41 +1000

    Update run script to choose a more simple docker tag to make deving more simple.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker build . -t pairing-tool-server:node-alpha-1 && docker run -it -p 4001:4001 pairing-tool-server:node-alpha-1
+docker build . -t tyro/pairing-tool-websocket-server:local && docker run -it -p 4001:4001 tyro/pairing-tool-websocket-server:local
